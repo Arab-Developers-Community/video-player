f065a74d7c72f53173395d73405fa34e29d9a557
Mohammad Rahhal <mohammed.ayman.rahhak123@gmail.com>
2024-04-27 20:42:08 +0300

    creting gui for main page

--- main.py
+++ main.py
@@ -4,65 +4,71 @@ import cv2
 import threading
 import queue
 from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfilename
+import customtkinter as ctk
+import tkinter.messagebox as tkmb
+# Selecting GUI theme - dark, light , system (for system default)
+ctk.set_appearance_mode("dark")
+
+# Selecting color theme - blue, green, dark-blue
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+window_width = app.winfo_screenwidth()
+window_height = app.winfo_screenheight()
+app.geometry("%dx%d" % (window_width, window_height))
+app.title("Modern Login UI using Customtkinter")
+
+f_path = None
+
+# def login():
+#     username = "Geeks"
+#     password = "12345"
+#     new_window = ctk.CTkToplevel(app)
+#
+#     new_window.title("New Window")
+#
+#     new_window.geometry("350x150")
+#
+#     if user_entry.get() == username and user_pass.get() == password:
+#         tkmb.showinfo(title="Login Successful", message="You have logged in Successfully")
+#         ctk.CTkLabel(new_window, text="GeeksforGeeks is best for learning ANYTHING !!").pack()
+#     elif user_entry.get() == username and user_pass.get() != password:
+#         tkmb.showwarning(title='Wrong password', message='Please check your password')
+#     elif user_entry.get() != username and user_pass.get() == password:
+#         tkmb.showwarning(title='Wrong username', message='Please check your username')
+#     else:
+#         tkmb.showerror(title="Login Failed", message="Invalid Username and password")
+def browse():
+    f_path = askopenfilename(initialdir="/", title="Select File",
+                             filetypes=(("Video files", "*.mp4"), ("All Files", "*.*")))
+    f_name_label.configure(text='file selected: ' + f_path)
+def start():
+    return
+frame = ctk.CTkFrame(master=app)
+frame.pack(pady=20, padx=40, fill='both', expand=True)
+
+label = ctk.CTkLabel(master=frame, text='Select a video')
+label.pack(pady=12, padx=10)
+label = ctk.CTkLabel(master=frame, text='NOTE: This video player only supports mp4 files')
+label.pack(pady=12, padx=10)
+
+button = ctk.CTkButton(master=frame, text='Browse', command=browse)
+button.pack(pady=12, padx=10)
+
+f_name_label = ctk.CTkLabel(master=frame, text='file selected: None')
+f_name_label.pack(pady=12, padx=10)
+
+label = ctk.CTkLabel(master=frame, text='Select filters:')
+label.pack(pady=12, padx=10)
+checkbox = ctk.CTkCheckBox(master=frame, text='Nudity')
+checkbox.pack(pady=12, padx=10)
+
+checkbox = ctk.CTkCheckBox(master=frame, text='Kissing')
+checkbox.pack(pady=12, padx=10)
+
+button = ctk.CTkButton(master=frame, text='Start Video', command=start)
+button.pack(pady=12, padx=10)
 
 
-class VideoPlayer:
-    def __init__(self, root, video):
-        self.root = root
-        self.root.title("Video Player")
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
-        self.video_label = ttk.Label(self.frame)
-        self.video_label.pack()
-        self.video_capture = cv2.VideoCapture(video)
-        self.frame_count = int(self.video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.buffer = queue.Queue()
-        self.frames_displayed = 0
-
-        self.create_threads()
-        self.update()
-
-    def update(self):
-
-        if self.frames_displayed == self.frame_count:
-            # If the video ends, you can add logic to handle this event
-            self.video_capture.release()
-
-        self.display_frame(cv2.cvtColor(self.buffer.get(), cv2.COLOR_BGR2RGB))
-        self.frames_displayed = self.frames_displayed + 1
-
-        self.root.after(33, self.update)
-
-    def start_checking(self, lock):
-        while True:
-            lock.acquire()
-
-            ret, frame = self.video_capture.read()
-
-            if ret:
-                # this code to detect nudity **NEED TO BE UPDATED
-                self.buffer.put(frame)
-                ##################################################
-            else:
-                # If the video ends, you can add logic to handle this event
-                threading.current_thread().join()
-
-            lock.release()
-
-    def create_threads(self, threads_num=4):
-        lock = threading.Lock()
-        for i in range(0, threads_num):
-            thread = threading.Thread(target=VideoPlayer.start_checking , args=(self,lock,))
-            thread.start()
-
-    def display_frame(self, frame):
-        image = Image.fromarray(frame)
-        photo = ImageTk.PhotoImage(image=image)
-        self.video_label.config(image=photo)
-        self.video_label.image = photo
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    player = VideoPlayer(root, "video.mp4")
-    root.mainloop()
+app.mainloop()
